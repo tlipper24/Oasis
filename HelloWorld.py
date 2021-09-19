6f65b05473dfdcb958d9038e2667f4a644b0d320
tlipper24 <90801695+tlipper24@users.noreply.github.com>
2021-09-18 20:02:20 -0700

    Create HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -4,8 +4,11 @@ def hello():
 def GameStart():
     print('When exploring the map of Fallout 3 you encounter this strange place called "Oasis".')
     print('On aproaching Oasis, a man who goes by the name of "Tree Father Birch" asks you to follow him')
-    print('Birch explains that "He" has summoned you, but is extremely vague about who or what "He" is.')
+    print('Birch explains that "He" has summoned you, but doesent say exactly who or what "He" is.')
     print('Leading you to their village Birch explains that he is the leader of "The Treeminders"')
+    print('
+    
+    
     print('On arival to their village The Treeminders lead you into the Grove, where they introduce you to a tree named Harold.')
     print('Birch informs you that harrold is the one who asked for you to be brought in')
     print('They all await to see what you chose to say to harrold')
