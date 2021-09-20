86570d0cfbf9f43df99ee2a5209546d0218868ae
tlipper24 <90801695+tlipper24@users.noreply.github.com>
2021-09-20 11:38:47 -0700

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -4,15 +4,17 @@ def HelloWorld():
 def CreditWhereCreditIsDue():
     print ('This text adventure is heavily based off of the "Oasis" side quest from Fallout 3')
     print ("I did not come up with the plot, characters, decisions, or anything else displayed in this text adventure")
-    print ("All credit for the plot goes to Bethesda (the original developers of Fallout 3) and the Fallout 3 fandom website which I used as reference material")
+    print ("All credit for the plot goes to Bethesda (the original developers of Fallout 3), the Fallout 3 fandom website, and the YouTube videos (by Oxhorn) which I used as reference material")
     print ("Reference material here ---> https://fallout.fandom.com/wiki/Oasis_(quest)")
 CreditWhereCreditIsDue()
 def GameStart():
     print('When exploring the map of Fallout 3 you encounter this strange place called "Oasis".')
-    print('On aproaching Oasis, a man who goes by the name of "Tree Father Birch" asks you to follow him')
+    print('On aproaching Oasis, a strange man who goes by the name of "Tree Father Birch" asks you to follow him')
     print('Birch explains that "He" has summoned you, but doesent say exactly who or what "He" is.')
+    
+    
     print('Leading you to their village Birch explains that he is the leader of "The Treeminders"')
-    print('
+    print('The Treeminders are a group of people who worship trees
     
     
     print('On arival to their village The Treeminders lead you into the Grove, where they introduce you to a tree named Harold.')
